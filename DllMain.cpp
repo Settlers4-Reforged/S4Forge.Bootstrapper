032fe4f9f8fca8af9cb9e2b8a1912cbcf0b30b6d
WizzardMaker <wizzardmaker@gmail.com>
2023-01-19 18:20:00 +0100

    added check for correct application start

--- DllMain.cpp
+++ DllMain.cpp
@@ -1,5 +1,9 @@
 #include "pch.h"
 #include "ManagedLoader.h"
+#include <filesystem>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <tlhelp32.h>
 
 #pragma managed(push, off)
 S4API ModAPI;
@@ -20,6 +24,26 @@ static bool Init() {
     return true;
 }
 
+HANDLE GetParentProcess() {
+    HANDLE Snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    PROCESSENTRY32 ProcessEntry = {};
+    ProcessEntry.dwSize = sizeof(PROCESSENTRY32);
+
+    if (Process32First(Snapshot, &ProcessEntry)) {
+        DWORD CurrentProcessId = GetCurrentProcessId();
+
+        do {
+            if (ProcessEntry.th32ProcessID == CurrentProcessId)
+                break;
+        } while (Process32Next(Snapshot, &ProcessEntry));
+    }
+
+    CloseHandle(Snapshot);
+
+    return OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, ProcessEntry.th32ParentProcessID);
+}
+
 extern "C" BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
     //MessageBoxA(nullptr, "NetModAPI DEBUG", "NetModAPI", 0);
     switch (ul_reason_for_call) {
@@ -28,6 +52,22 @@ extern "C" BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPV
         if (GetAsyncKeyState(VK_F2)) {
             return TRUE;
         }
+        
+        auto parent_handle = GetParentProcess();
+        wchar_t process_name[1024] = { 0 };
+        DWORD   process_name_size = 1024;
+        QueryFullProcessImageNameW(parent_handle, 0, process_name, &process_name_size);
+        std::filesystem::path process_file_path(process_name);
+
+        const std::string parentName = process_file_path.filename().string();
+        
+        // abort if we're not called from Ubisoft Launcher or Internal Call... (Prevents loading in first call)
+        if (_stricmp(parentName.c_str(), "UbisoftGameLauncher.exe") != 0 &&
+            _stricmp(parentName.c_str(), "UbisoftGameLauncher64.exe") != 0 &&
+            _stricmp(parentName.c_str(), "S4_Main.exe") != 0) {
+            return TRUE;
+        }
+
 
 #ifndef PUBLIC
         if (GetAsyncKeyState('Q')) {
@@ -40,6 +80,7 @@ extern "C" BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPV
     }
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
+    default:
         break;
     case DLL_PROCESS_DETACH:
         CleanUp();
